4be6d3eef23c4e46030ac0fa497dd3a3723bd190
Anoop Singh <therain0605@gmail.com>
2022-01-30 12:36:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// var trnsitCheck = 0;
+
+// window.onscroll = function(e) {
+
+// if (window.scrollY > 320) {
+
+//     if (trnsitCheck != 0) {
+//         return;
+//     }
+//     let magnetMoveable = document.getElementsByClassName("absolute-pos")[0];
+//     magnetMoveable.style.transition = "2500ms linear";
+//     magnetMoveable.style.right = "400px";
+//     trnsitCheck = 1;
+// }
+
+
+// }
